7dddafa3c982a81a61fb9f065cafc8285268e84f
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-03-07 13:25:08 +0000

    the files

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@ module "my_public_subnet_calling_module" {
     count = ( length(var.public_subnets) > 0 ) ? length(var.public_subnets) : 0
     # [ "10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28" ]
     #           0            1                 2
-    source     = "github.com:vysaghkrishnan/terraform-modules/subnet/module"
+    source     = "github.com/vysaghkrishnan/terraform-modules/subnet/module"
     vpc_id = module.myvpc_calling_module.outputs.myvpc_id
     subnet_cidr_block = var.public_subnets[count.index]
     project = "INFRA_TRAINING"  

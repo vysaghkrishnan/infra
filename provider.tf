9dc451dd3cf3dda21bc8446ffee1b80769a6b698
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-03-07 12:57:27 +0000

    the files

--- provider.tf
+++ provider.tf
@@ -9,11 +9,13 @@ terraform {
       key = "mystate.tfstate"
       region = "eu-west-2"
   }
-
+  }
+terraform {
   required_providers {
     aws = {
         source = hashicorp/aws
         version = "~> 3.0"
     }
   }
+
 }
